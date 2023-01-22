4cdb8d40362c57998b0f10775cf708d6eaf3446b
Sergey <sergeykhlynovskiy@gmail.com>
2023-01-22 02:47:55 -0700

    moved quacksite to new repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Me from './pages/index';
-import Quack from './pages/quacksite';
 
   
 function App() {
@@ -11,8 +10,6 @@ function App() {
         <Router>
             <Routes>
                 <Route exact path='/' element={<Me />} />
-                <Route path='/stick-game' element={<Me />} />
-                <Route path='/quacksite' element={<Quack />} />
             </Routes>
         </Router>
     );
